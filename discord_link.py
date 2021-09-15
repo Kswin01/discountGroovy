1f8df2e89fcedf7988f87a226b7039dacc04de51
Krishnan Winter <z5258205@ad.unsw.edu.au>
2021-09-15 22:00:36 +1000

    Added in youtube searching, pausing and resuming

--- discord_link.py
+++ discord_link.py
@@ -1,4 +1,4 @@
-# bot.py
+# discountGroovy.py
 import os
 
 import discord
@@ -6,8 +6,10 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio
+from youtubesearchpython import VideosSearch
 import youtube_dl
 import ffmpeg
+import re
 
 #load_dotenv()
 TOKEN = 'ODg3NTc4OTY2OTE4MDUzOTQ5.YUGMVA.jJD-vBv0_sHlNQ3iC-KGlLg_GYw'
@@ -16,12 +18,24 @@ bot = commands.Bot(command_prefix='?')
 
 
 @bot.command()
-async def play(ctx, url):
-    msg = 'I really should be playing something here'
+async def play(ctx, *args):
+    msg = 'enjoy your shite song'
     
+    url = args[0]
+
+    # Checking if the url is a youtube link, if not then use youtubesearchpython
+    if not re.match(r'.*www.youtube.*', url):
+        # Join list into args
+        url = ' '.join(args)
+        tempSearch = VideosSearch(url, limit = 1)
+        tempURL = tempSearch.result()['result'][0]['link']
+        url = tempURL
+
+    # Connect the bot to the channel
     channel = ctx.author.voice.channel
     await channel.connect()
 
+    # Play the youtube audio, taken from stackoverflow
     voice = get(bot.voice_clients, guild=ctx.guild)
     ydl_opts = {
         'format': 'worst',
@@ -31,25 +45,49 @@ async def play(ctx, url):
             'preferredquality': '192',
         }],
     }
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
     for file in os.listdir("./"):
         if file.endswith(".mp3"):
             os.rename(file, 'song.mp3')
+
     voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    voice.volume = 100
+
+    voice.volume = 75
+    
     voice.is_playing()
 
-    await ctx.send(url)
+    await ctx.send(msg)
 
 @bot.command()
 async def stop(ctx):
     msg = "I really should stop playing shouldn't I?"
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
     await ctx.send(msg)
 
 @bot.command()
 async def skip(ctx):
     msg = "Alright i'll skip this one"
+    await ctx.send(msg)
+
+@bot.command()
+async def pause(ctx):
+    msg = "Alright alright give me a fucking second"
+    await ctx.send(msg)
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    voice.pause()
+
+@bot.command()
+async def resume(ctx):
+    msg = "There you fucking go you mut"
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    voice.resume()
+
     await ctx.send(msg)
 
 @bot.command()
